df1931e4eae113c65277118fcb03c708e3398811
Arisu Wonderland <arisuchr@riseup.net>
2024-10-30 16:45:15 +0300

    Fish: Update functions

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,8 +1,8 @@
 function fish_greeting
-	if [ -z "$VIRTUAL_ENV" ]; and [ $SHLVL -eq 1 ]; and [ ! $FISH_GREETING ]
+	if [ -z "$VIRTUAL_ENV" ]; and [ $SHLVL -lt 3 ]; and [ ! $FISH_GREETING ]
 		neofox cat
 		set -g FISH_GREETING 1
-	else if [ ! $TERM_PROGRAM ]
+	else if [ ! $TERM_PROGRAM ] 
 		set_color cyan
 		echo " > This is a #$SHLVL shell."
 		set_color normal
--- .config/fish/functions/pyinit.fish
+++ .config/fish/functions/pyinit.fish
@@ -1,6 +1,8 @@
 function pyinit
+    set -l template_dir "$HOME/python-template"
+
     echo ':: Copying template files...'
-    cp -R ~/python-template/$(/bin/ls -1A ~/python-template/ -I venv -I '*.lock' -I .git) .
+    cp -R $template_dir/$(/bin/ls -1A $template_dir -I venv -I '*.lock' -I .git) .
     echo ':: Creating "venv"...'
     mkv
 end
